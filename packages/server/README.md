33fc1d067561a083703ec8a90e69d8c3a369cfd5
Csaba Nagy <33700462+csaba-nagy@users.noreply.github.com>
2022-08-05 09:32:39 +0200

    Set up postgreSQL (#8)
    * feat(postgresql): add db service to devcontainer (#7)
    
    * feat(postgresql): add config (#7)
    
    * docs(readme): update content (#7)
    
    * fix(devcontainer): env path (#7)
    
    * docs(readme): update (#7)

---
+++ packages/server/README.md
@@ -0,0 +1,1 @@
+# @blog-adonis/server package
--- packages/server/start/routes.ts
+++ packages/server/start/routes.ts
@@ -18,7 +18,14 @@
 |
 */
 
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
 import Route from '@ioc:Adonis/Core/Route'
+
+// check db connection
+Route.get('health', async ({ response }) => {
+  const report = await HealthCheck.getReport()
+  return report.healthy ? response.ok(report) : response.badRequest(report)
+})
 
 Route.get('/', async () => {
   return { hello: 'world' }
